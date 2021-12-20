2442d998e6642685256bc37139aafd36c46a49dc
kazuhito_m <kazuhito.sumpic@gmail.com>
2021-12-20 23:33:20 +0900

    refacoor: JUnit5からpublicクラスでなくてもよくなったので、そうする。’

--- youkai-console-java/src/test/java/com/github/kazuhitom/youkaicheckdigittry/YoukaiPasswordAttackerTest.java
+++ youkai-console-java/src/test/java/com/github/kazuhitom/youkaicheckdigittry/YoukaiPasswordAttackerTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Disabled
-public class YoukaiPasswordAttackerTest {
+class YoukaiPasswordAttackerTest {
     private final YoukaiPasswordAttacker sut = new YoukaiPasswordAttacker(true);
 
     @Test
