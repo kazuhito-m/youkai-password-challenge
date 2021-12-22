0039e3012d0e8fb98a4f0c9dc935b53a3be06509
kazuhito_m <kazuhito.sumpic@gmail.com>
2021-12-22 13:24:51 +0900

    refactor: CheckDigitCalculatorVar2 を CheckDigitCalculator として上書き。

--- youkai-console-java/src/main/java/com/github/kazuhitom/youkaicheckdigittry/checkdigit/CheckDigitCalculator.java
+++ youkai-console-java/src/main/java/com/github/kazuhitom/youkaicheckdigittry/checkdigit/CheckDigitCalculator.java
@@ -4,163 +4,55 @@ import com.github.kazuhitom.youkaicheckdigittry.checkdigit.state.A31F;
 import com.github.kazuhitom.youkaicheckdigittry.checkdigit.state.AttackCharacters;
 
 public class CheckDigitCalculator {
-    private int A = 0;
-
     public A31F calculate(AttackCharacters password) {
-        final A31F currentCheckDigit = A31F.prototypeOf(password.charLength());
-        final int D87F = subroutineD8C0(currentCheckDigit, password.getOf(0));
-        D880(currentCheckDigit, password, D87F);
-        return currentCheckDigit;
+        A31F a31f = A31F.prototypeOf(password.charLength());
+        for (int charPosition = 0; charPosition < password.charLength(); charPosition++) {
+            D8C0(password, a31f, charPosition);  // 文字数分だけ演算をカウント
+        }
+        return a31f;
     }
 
-    private int subroutineD8C0(A31F a31f, int targetCharCode) {
-        A = targetCharCode;
-        for (int i = 0; i < 8; i++) {
-            A = A << 1;
+    private void D8C0(final AttackCharacters password, final A31F a31f, final int targetCharPosition) {
+        final int targetCharCode = password.getOf(targetCharPosition);
+        int shiftedCode = targetCharCode;
+        for (int y = 0; y < 8; y++) {
+            int A01 = shiftedCode << 1;
 
-            int C1 = 0;
-            if (A > 0xFF) {
+            final int C1;
+            if (A01 > 0xFF) {
                 C1 = 1;
-                A = A & 0xFF;
-            }
-            int a1Work = A;
-            // 31F4と31F5を右1ビットローテート
-            int work1 = a31f.a31F4 & 0x01;
-            a31f.a31F4 = a31f.a31F4 >> 1;
-            a31f.a31F4 = a31f.a31F4 | (C1 << 7); // C0000000
-            C1 = work1;
-
-            int work2 = a31f.a31F5 & 0x01;
-            a31f.a31F5 = a31f.a31F5 >> 1;
-            a31f.a31F5 = a31f.a31F5 | (C1 << 7); // C0000000
-            C1 = work2;
-
-            //printf("ror %02X %02X\n",a31F4,a31F5);
-
-            A = 0xFF + C1;
-
-            if (A > 0xFF) {
-                A = 0;
-            }
-
-            A = A ^ 0xFF;
-            int a2Work = A;
-            A = A & 0x84;
-            A = A ^ a31f.a31F4;
-            a31f.a31F4 = A;
-            A = a2Work;
-            A = A & 0x08;
-            A = A ^ a31f.a31F5;
-            a31f.a31F5 = A;
-            A = a1Work;
-        }
-
-        A = targetCharCode; // ここまでで31F4と31F5算出完了
-
-//D8A4: // 31F7と31F8を生成(Complete)
-//        stackA.push(A);
-//        stackA.push(A);
-        final int a3Work = A;
-        final int a4Work = A;
-        A = a31f.a31F4;
-
-        int C3 = 0;
-        if (A >= 0xE5) {
-            C3 = 1;
-        } //C5の値でキャリーを生成
-//        A = stackA.pop();
-        A = a4Work;
-        A = A + a31f.a31F7 + C3;
-
-        int C4 = 0;
-        if (A > 0xFF) { // ADCのキャリー処理
-            A = A & 0xFF;
-            C4 = 1;
-        }
-        a31f.a31F7 = A;
-        A = a31f.a31F8;
-        A = A + a31f.a31F5 + C4;
-
-        int C5 = 0;
-        if (A > 0xFF) { // ADCのキャリー処理
-            A = A & 0xFF;
-            C5 = 1;
-        }
-        a31f.a31F8 = A;
-//        A = stackA.pop();
-        A = a3Work;
-
-        //D89B: // 31F9を生成(Complete)
-//        stackA.push(A);
-        final int a5Work = A;
-        A = A ^ a31f.a31F9;
-        a31f.a31F9 = A;
-//        A = stackA.pop();
-        A = a5Work;
-
-        // ここから下にまだバグがある
-
-        //D88F: // 31FAを生成
-//        stackA.push(A);
-        final int a6Work = A;
-        // 31FAをローテート
-        int work3 = a31f.a31FA & 0x01;
-        a31f.a31FA = a31f.a31FA >> 1;
-        a31f.a31FA = a31f.a31FA | (C5 << 7); // $31F8のCがここで入る
-
-        int C6 = work3;
-        A = A + a31f.a31FA + C6;
-
-        int C7 = 0;
-        if (A > 0xFF) { // ADCのキャリー処理
-            A = A & 0xFF;
-            C7 = 1;
-        }
-        a31f.a31FA = A;
-
-//        A = stackA.pop();
-        A = a6Work;
-        //D87F:
-        return A;
-    }
-
-    private void D880(A31F a31f, AttackCharacters password, int D87F) {
-        int C1 = 0;
-        for (int X = 0; X < a31f.charLength(); X++) {
-            // 文字数分だけ演算をカウント
-            if (X > 0) {
-                A = password.getOf(X);
-                D87F = subroutineD8C0(a31f, A);
-            }
-
-            // 31FBを生成
-            boolean Z;
-            do {
-                // Aを左ローテート
-                A = A << 1;
-                if (A > 0xFF) { // ADCのキャリー処理
-                    A = A & 0xFF;
-                    C1 = 1;
-                }
-                Z = A == 0; // 演算結果がゼロの時Z=true;
-
-//                stackA.push(A); // スタックに値を保存
-                final int a1Work = A;
-                A = a31f.a31FB;
-                A = A + C1;
-
+                A01 = A01 & 0xFF;
+            } else {
                 C1 = 0;
-                if (A > 0xFF) { // ADCのキャリー処理
-                    A = A & 0xFF;
-                    C1 = 1;
-                }
-                a31f.a31FB = A;
+            }
 
-                A = a1Work;
-            } while (!Z);   // ローテ終わるまでループ
-            //printf("a31FB=%x ",a31FB);
+            // 31F4と31F5を右1ビットローテート
+            final int C2 = a31f.rotateRightOneBit31F4(C1);
+            final int C3 = a31f.rotateRightOneBit31F5(C2);
 
-            A = D87F;
+            int A02 = 0xFF + C3;
+            if (A02 > 0xFF) {
+                A02 = 0;
+            }
+
+            a31f.calc31F4And31F5(A02);
+
+            shiftedCode = A01;
         }
+
+        // ここまでで31F4と31F5算出完了
+
+        // 31F7と31F8を生成(Complete)
+        // 31F9を生成(Complete)
+        final int C7 = a31f.calc31F4And31F5And31F9(targetCharCode);
+
+// ここから下にまだバグがある
+
+        // 31FAを生成
+        // 31FAをローテート
+        final int work3 = a31f.rotateRightOneBit31FA(C7); // $31F8のCがここで入る
+        final int C9 = a31f.calc31FA(targetCharCode, work3);
+
+        a31f.calc31FB(C9, targetCharCode);
     }
 }
--- youkai-console-java/src/test/java/com/github/kazuhitom/youkaicheckdigittry/checkdigit/CheckDigitCalculatorTest.java
+++ youkai-console-java/src/test/java/com/github/kazuhitom/youkaicheckdigittry/checkdigit/CheckDigitCalculatorTest.java
@@ -6,50 +6,52 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CheckDigitCalculatorTest {
-    private CheckDigitCalculator sut = new CheckDigitCalculator();
+    private final CheckDigitCalculator sut = new CheckDigitCalculator();
 
-    AttackCharacters toCode(String text) {
-        return AttackCharacters.withText(text);
+    private String calc(String passwordText) {
+        var password = AttackCharacters.withText(passwordText);
+        var calculated = sut.calculate(password);
+        return calculated.toString();
     }
 
     @Test
     public void 主要なパスワードからチェックディジット変換で出来る() {
-//        assertEquals("ED 26 08 EE 3D 23 1D 12", sut.calculate(toCode("SPEED-UP")).toString());
-//        assertEquals("5F 65 0C 8F 8B 22 76 13", sut.calculate(toCode("OHAYOUKAWADA")).toString());
-        assertEquals("9A 7D 09 9E B5 09 55 0C", sut.calculate(toCode("UDADAGAWA")).toString());
-        assertEquals("4A 89 0A 1F F5 28 7B 1C", sut.calculate(toCode("S.62.08.22")).toString());
-        assertEquals("86 C4 09 6B 32 22 E6 0E", sut.calculate(toCode("KOBAYASHI")).toString());
-        assertEquals("54 34 06 3C 10 0A 9F 08", sut.calculate(toCode("KAWADA")).toString());
-        assertEquals("63 B6 09 20 B4 08 EA 13", sut.calculate(toCode("PC-ENGINE")).toString());
-        assertEquals("46 60 03 61 EC 01 2D 05", sut.calculate(toCode("NEC")).toString());
-        assertEquals("70 74 06 BB D6 02 4E 0F", sut.calculate(toCode("MIZUNO")).toString());
-        assertEquals("3E 19 04 5C 5B 28 76 0C", sut.calculate(toCode("6809")).toString());
-        assertEquals("F9 41 07 AC E5 21 83 10", sut.calculate(toCode("MONITOR")).toString());
-        assertEquals("44 3D 09 9A 57 32 71 0F", sut.calculate(toCode("YAMASHITA")).toString());
-        assertEquals("63 95 07 6A B1 21 44 0E", sut.calculate(toCode("MICHIYO")).toString());
-        assertEquals("01 F7 04 54 94 22 E5 08", sut.calculate(toCode("YAGI")).toString());
-        assertEquals("08 3A 08 94 CB 2A 78 13", sut.calculate(toCode("YUKIHIKO")).toString());
-        assertEquals("4B 70 05 4D 96 3B 36 09", sut.calculate(toCode("KOMAI")).toString());
-        assertEquals("D4 77 08 94 CD 1B 6A 10", sut.calculate(toCode("KAZUHIKO")).toString());
-//        assertEquals("FA 68 08 8A 3A 1F 8D 12", sut.calculate(toCode("756-2311")).toString());
-        assertEquals("3B EA 05 6C D2 0A D8 08", sut.calculate(toCode("NAMCO")).toString());
-//        assertEquals("1C 6F 06 96 41 20 16 0C", sut.calculate(toCode("NAMCOT")).toString());
-        assertEquals("F2 4A 09 0A A2 12 76 10", sut.calculate(toCode("NAGAMATSU")).toString());
-        assertEquals("43 4C 05 3C 35 0A 93 08", sut.calculate(toCode("AKIRA")).toString());
-        assertEquals("45 D7 08 9F AD 38 92 0B", sut.calculate(toCode("NAUSICAA")).toString());
-        assertEquals("04 49 06 87 39 33 C4 0A", sut.calculate(toCode("LAPUTA")).toString());
-        assertEquals("29 62 05 89 BD 35 64 0E", sut.calculate(toCode("68000")).toString());
-        assertEquals("F8 7D 04 5B 4B 08 A9 09", sut.calculate(toCode("6502")).toString());
-        assertEquals("00 08 03 22 88 20 22 03", sut.calculate(toCode("HAL")).toString());
-        assertEquals("00 51 03 3A E9 08 23 07", sut.calculate(toCode("KID")).toString());
-        assertEquals("5D 50 0C FD B4 13 63 17", sut.calculate(toCode("KUMI.HANAOKA")).toString());
-//        assertEquals("04 76 10 32 D1 21 27 1B", sut.calculate(toCode("HARUHISA.UDAGAWA")).toString());
+        assertEquals("ED 26 08 EE 3D 23 1D 12", calc("SPEED-UP"));
+        assertEquals("5F 65 0C 8F 8B 22 76 13", calc("OHAYOUKAWADA"));
+        assertEquals("9A 7D 09 9E B5 09 55 0C", calc("UDADAGAWA"));
+        assertEquals("4A 89 0A 1F F5 28 7B 1C", calc("S.62.08.22"));
+        assertEquals("86 C4 09 6B 32 22 E6 0E", calc("KOBAYASHI"));
+        assertEquals("54 34 06 3C 10 0A 9F 08", calc("KAWADA"));
+        assertEquals("63 B6 09 20 B4 08 EA 13", calc("PC-ENGINE"));
+        assertEquals("46 60 03 61 EC 01 2D 05", calc("NEC"));
+        assertEquals("70 74 06 BB D6 02 4E 0F", calc("MIZUNO"));
+        assertEquals("3E 19 04 5C 5B 28 76 0C", calc("6809"));
+        assertEquals("F9 41 07 AC E5 21 83 10", calc("MONITOR"));
+        assertEquals("44 3D 09 9A 57 32 71 0F", calc("YAMASHITA"));
+        assertEquals("63 95 07 6A B1 21 44 0E", calc("MICHIYO"));
+        assertEquals("01 F7 04 54 94 22 E5 08", calc("YAGI"));
+        assertEquals("08 3A 08 94 CB 2A 78 13", calc("YUKIHIKO"));
+        assertEquals("4B 70 05 4D 96 3B 36 09", calc("KOMAI"));
+        assertEquals("D4 77 08 94 CD 1B 6A 10", calc("KAZUHIKO"));
+        assertEquals("FA 68 08 8A 3A 1F 8D 12", calc("756-2311"));
+        assertEquals("3B EA 05 6C D2 0A D8 08", calc("NAMCO"));
+        assertEquals("1C 6F 06 96 41 20 16 0C", calc("NAMCOT"));
+        assertEquals("F2 4A 09 0A A2 12 76 10", calc("NAGAMATSU"));
+        assertEquals("43 4C 05 3C 35 0A 93 08", calc("AKIRA"));
+        assertEquals("45 D7 08 9F AD 38 92 0B", calc("NAUSICAA"));
+        assertEquals("04 49 06 87 39 33 C4 0A", calc("LAPUTA"));
+        assertEquals("29 62 05 89 BD 35 64 0E", calc("68000"));
+        assertEquals("F8 7D 04 5B 4B 08 A9 09", calc("6502"));
+        assertEquals("00 08 03 22 88 20 22 03", calc("HAL"));
+        assertEquals("00 51 03 3A E9 08 23 07", calc("KID"));
+        assertEquals("5D 50 0C FD B4 13 63 17", calc("KUMI.HANAOKA"));
+        assertEquals("04 76 10 32 D1 21 27 1B", calc("HARUHISA.UDAGAWA"));
 
         // 今回見つかった三種
 
-//        assertEquals("64 98 0B 15 91 18 B1 15", sut.calculate(toCode("HENTAIOSUGI")).toString());
-//        assertEquals("DC D9 08 A3 E3 17 28 15", sut.calculate(toCode("818-6104")).toString());
-        assertEquals("65 94 0E AC E9 07 33 25", sut.calculate(toCode("534-030.565.81")).toString());
-        assertEquals("65 94 0E AC E9 07 33 25", sut.calculate(toCode("009n251.mn3202")).toString());
+        assertEquals("64 98 0B 15 91 18 B1 15", calc("HENTAIOSUGI"));
+        assertEquals("DC D9 08 A3 E3 17 28 15", calc("818-6104"));
+        assertEquals("65 94 0E AC E9 07 33 25", calc("534-030.565.81"));
+        assertEquals("65 94 0E AC E9 07 33 25", calc("009n251.mn3202"));
     }
 }
