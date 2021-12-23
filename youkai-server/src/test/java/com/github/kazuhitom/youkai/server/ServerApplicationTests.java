b3917913a6fb5b0853104ba24f05b8ebc8459b73
kazuhito_m <kazuhito.sumpic@gmail.com>
2021-12-23 23:13:59 +0900

    implement: SpringBootなサーバプロジェクトを追加。

---
+++ youkai-server/src/test/java/com/github/kazuhitom/youkai/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kazuhitom.youkai.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
