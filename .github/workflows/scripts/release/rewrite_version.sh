f39bc647ba4f1ed4e1229f6e84ebf4ee016ba99d
kazuhito_m <kazuhito.sumpic@gmail.com>
2021-12-25 03:40:33 +0900

    implement: バージョンを変更するスクリプトを作成。

---
+++ .github/workflows/scripts/release/rewrite_version.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#
+# ビルド前に「自身アプリケーションのバージョンを書き換える」スクリプト。
+#
+# 引数
+# 1. バージョンの文字列
+#
+# 前提
+# - このリポジトリのトップディレクトリから実行される
+#
+
+version=${1}
+
+general_setting_path='./build.gradle'
+sed -e "s/version .*/version '${version}'" ${general_setting_path} > ${general_setting_path}.tmp
+mv ${general_setting_path}.tmp ${general_setting_path}
+
+front_setting_path='./youkai-front/package.json'
+sed -e "s/version.*/version\": \"${version}\",/g" ${front_setting_path} > ${front_setting_path}.tmp
+mv ${front_setting_path}.tmp ${front_setting_path}
