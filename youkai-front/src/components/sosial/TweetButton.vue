a9623a9fbeb2d4b25ea82035573b1b4fe2f693ce
kazuhito_m <kazuhito.sumpic@gmail.com>
2021-12-26 15:50:07 +0900

    refactor: Twiterリンクは、今のURLをもってくように。

--- youkai-front/src/components/sosial/TweetButton.vue
+++ youkai-front/src/components/sosial/TweetButton.vue
@@ -3,6 +3,7 @@
     <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
     <a href="https://twitter.com/share" 
         class="twitter-share-button" 
+        :data-url="nowUrl()" 
         data-text="Youkai Password Challenge" 
         data-size="large" 
         data-hashtags="隠しパスワード解析選手権">
@@ -10,3 +11,14 @@
     </a> 
 </span>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component
+export default class TweetButton extends Vue {
+    private nowUrl(): string {
+        return window.location.href;
+    }
+}
+</script>
