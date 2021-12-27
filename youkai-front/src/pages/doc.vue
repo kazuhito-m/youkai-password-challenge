2b52b7b0de146ce397072d958424e494a39a9d9a
kazuhito_m <kazuhito.sumpic@gmail.com>
2021-12-28 01:38:21 +0900

    refactor: MarkDown記事システムの作り込みと、記事一つ追加。

--- youkai-front/src/pages/doc.vue
+++ youkai-front/src/pages/doc.vue
@@ -1,23 +1,47 @@
 <template>
-  <ul>
-    <li v-for="article in blog" :key="article.slug">
-      <nuxt-link :to="'/tech/' + article.slug">
-        <time :datetime="article.createdAt">
-          {{ $dateFns.format(new Date(article.createdAt), 'yyyy/MM/dd') }}
-        </time>
-        <p>{{ article.title }}</p>
-      </nuxt-link>
-    </li>
-  </ul>
+  <v-layout>
+    <v-container>
+      <v-row justify="center" align="center">
+        <v-col cols="12" sm="8" md="8">
+          <v-card>
+            <v-card-title class="headline">
+              記事
+            </v-card-title>
+            <v-list dense>
+              <v-list-item
+                v-for="article in blog"
+                :key="article.slug"
+              >
+                <v-list-item-content :class="{ 'blue--text': sortBy === article.createdAt }">
+                  {{ article.createdAt }}:
+                </v-list-item-content>
+                <v-list-item-content
+                  class="align-end"
+                  :class="{ 'blue--text': sortBy === key }"
+                >
+                  <nuxt-link :to="'/tech/' + article.slug">
+                    {{ article.title }}
+                  </nuxt-link>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-layout>
 </template>
  
 <script lang="ts">
 import { Context } from '@nuxt/types'
+import moment from 'moment';
 
 export default {
-  async asyncData({$content,params }: Context) {
+  async asyncData({ $content, params }: Context) {
     // 記事を全て取得（作成日で降順にソート）
-    const blog = await $content('tech').sortBy('createdAt', 'desc').fetch()
+    const blog = await $content('tech')
+      .sortBy('createdAt', 'desc')
+      .fetch()
     return {
       blog,
     }
--- youkai-front/src/pages/tech/_slug.vue
+++ youkai-front/src/pages/tech/_slug.vue
@@ -1,9 +1,20 @@
 <template>
-  <main>
-    <h1>{{ article.title }}</h1>
-    <nuxt-content :document="article" />
-  </main>
-</template>
+  <v-layout>
+    <v-container>
+      <v-row justify="center" align="center">
+        <v-col cols="12" sm="8" md="8">
+          <v-card>
+            <v-card-title class="headline">
+              {{ article.title }}
+            </v-card-title>
+            <v-card-text>
+              <nuxt-content :document="article" />
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-layout></template>
 
 <script lang="ts">
 import { Context } from '@nuxt/types'
