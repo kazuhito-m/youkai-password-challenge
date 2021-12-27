0ff51f379f9a97ea15f695b4e21b8a9c34680108
kazuhito_m <kazuhito.sumpic@gmail.com>
2021-12-28 01:42:21 +0900

    refactor: 記事一覧リンクを着ける。

--- youkai-front/src/pages/tech/_slug.vue
+++ youkai-front/src/pages/tech/_slug.vue
@@ -7,6 +7,9 @@
             <v-card-title class="headline">
               {{ article.title }}
             </v-card-title>
+            <v-card-subtitle style="cursor: pointer" @click="$router.push('/doc')">
+              記事一覧へ
+            </v-card-subtitle>
             <v-card-text>
               <nuxt-content :document="article" />
             </v-card-text>
@@ -14,7 +17,8 @@
         </v-col>
       </v-row>
     </v-container>
-  </v-layout></template>
+  </v-layout>
+</template>
 
 <script lang="ts">
 import { Context } from '@nuxt/types'
@@ -23,6 +27,6 @@ export default {
   async asyncData({ $content, params }: Context) {
     const article = await $content('tech', params.slug).fetch()
     return { article }
-  }
+  },
 }
-</script>
+</script>git
