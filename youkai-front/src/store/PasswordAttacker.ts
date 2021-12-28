da55a431b801a1f0043e44d9fbe672320cb5ead7
kazuhito_m <kazuhito.sumpic@gmail.com>
2021-12-28 15:37:08 +0900

    implemnt: 非同期のおおまかな形を作る。。

--- youkai-front/src/store/PasswordAttacker.ts
+++ youkai-front/src/store/PasswordAttacker.ts
@@ -1,4 +1,4 @@
-import { Module, VuexModule, Action, Mutation } from 'vuex-module-decorators'
+import { Module, VuexModule, Action, Mutation } from 'vuex-module-decorators';
 import AttackPasswordRange from '@/domain/youkai/attack/AttackPasswordRange';
 
 @Module({
@@ -8,9 +8,20 @@ import AttackPasswordRange from '@/domain/youkai/attack/AttackPasswordRange';
 })
 export default class PasswordAttacker extends VuexModule {
     private executing = false;
+    private nowStartPosition = "";
+    private nowEndPosition = "";
+    private progressText = "";
+    private foundPassswords = "";
+
+    private nickName = "";
+    private fromPassword = "";
 
     public get nowExecuting(): boolean {
         return this.executing;
+    }
+
+    public get nowFromPassword(): string {
+        return this.fromPassword;
     }
 
     @Mutation
@@ -22,10 +33,30 @@ export default class PasswordAttacker extends VuexModule {
     public execute(passwordRange: AttackPasswordRange): void {
         console.log(passwordRange);
         this.changeExecuteState(true);
+        new Promise((resolve, reject) => {
+            try {
+                if (this.attack(passwordRange)) resolve(0);
+                else reject;
+            } catch (e) {
+                reject()
+            }
+        }).then(() => {
+            this.changeExecuteState(false);
+        }).catch(e => {
+            this.changeExecuteState(false);
+        });
     }
 
     @Action({ rawError: true })
     public cancel(): void {
         this.changeExecuteState(false);
+    }
+
+    private attack(passwordRange: AttackPasswordRange): boolean {
+
+
+
+
+        return true;
     }
 }
