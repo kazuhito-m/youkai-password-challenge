9ae83f0424aaf8d8b53e39ab9607fc91569e29d5
kazuhito_m <kazuhito.sumpic@gmail.com>
2021-12-31 06:04:59 +0900

    doc: 古い記述を修正。

--- youkai-front/src/components/infomation/MarkdownText.vue
+++ youkai-front/src/components/infomation/MarkdownText.vue
@@ -21,7 +21,7 @@
 
 ### Hit済パスワード分析・探索系
 
-[@tanzaku_sub](https://twitter.com/tanzaku_sub) さんが掘り当てた「1100万件の当たりパスワード」に「意味在るものがあるか」「正解っぽいものがあるか」を検索・分析するためのツール群です。
+[@tanzaku_sub](https://twitter.com/tanzaku_sub) さんが掘り当てた「4300万件の当たりパスワード」に「意味在るものがあるか」「正解っぽいものがあるか」を検索・分析するためのツール群です。
 
 - [kei／発見されたパスワードの管理・変換](https://github.com/kmikage/yokai-password)
 - [水咲／ YokaiSearcher(tanzaku氏発見の424万件パスワード探査ツール)](https://github.com/Misaki0331/YokaiSearcher)
