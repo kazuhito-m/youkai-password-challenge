354321f100d6b5d780e6e1cea2460127bd830053
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-01-01 19:48:14 +0900

    bugfix: SQLの間違いを修正。

--- env/local/postgres/init_sql/01_createdb.sql
+++ env/local/postgres/init_sql/01_createdb.sql
@@ -1,3 +1,3 @@
 CREATE USER youkai_user WITH SUPERUSER PASSWORD 'youkai_password';
 CREATE DATABASE youkai_password_challenge ENCODING 'UTF8' LC_COLLATE 'C' TEMPLATE 'template0' OWNER 'youkai_user';
-ALTER DATABASE public SET timezone TO 'Asia/Tokyo';
+ALTER DATABASE youkai_password_challenge SET timezone TO 'Asia/Tokyo';
