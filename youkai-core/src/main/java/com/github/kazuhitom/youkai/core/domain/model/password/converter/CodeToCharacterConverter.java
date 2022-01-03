e90b8f0cf204ce33da621f2e2b36b2ef23f84dc6
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-01-03 16:04:43 +0900

    implement: CodeToCharacterConverterをTypeScript側から移植してくる。

--- youkai-core/src/main/java/com/github/kazuhitom/youkai/core/domain/model/password/converter/CodeToCharacterConverter.java
+++ youkai-core/src/main/java/com/github/kazuhitom/youkai/core/domain/model/password/converter/CodeToCharacterConverter.java
@@ -1,6 +1,10 @@
 package com.github.kazuhitom.youkai.core.domain.model.password.converter;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
 
 /**
  * 文字コード変換テーブル。
@@ -24,6 +28,15 @@ public class CodeToCharacterConverter {
         return -1;
     }
 
+    public int reverceConvert(String passwordChar) {
+        int oneChar = passwordChar.charAt(0); // 念の為
+        for (int i = 0; i < this.codeToChar.length; i++) {
+            if (this.codeToChar[i] == oneChar) return i;
+        }
+        return -1;
+    }
+
+
     public int incrementCode(int code) {
         int index = incrementNextCodeTable.length - 1;
         if (code <= index) index = code;
@@ -34,8 +47,20 @@ public class CodeToCharacterConverter {
         return incrementNextCodeTable[incrementNextCodeTable.length - 1];
     }
 
+    public int maxCode() {
+        return Arrays.stream(this.incrementNextCodeTable)
+                .min()
+                .getAsInt();
+    }
+
     public boolean isInvalidCharCode(int code) {
         return INVALID_CHAR == convert(code);
+    }
+
+
+    public boolean isInvalidChar(String oneChar) {
+        if (CodeToCharacterConverter.equalInvalidChar(oneChar)) return true;
+        return !this.charTable.contains(oneChar);
     }
 
     private int[] createIncrementNextCodeTable(char[] codeToChar) {
@@ -52,6 +77,44 @@ public class CodeToCharacterConverter {
         }
 
         return result;
+    }
+
+    public int randomCode() {
+        String validChars = this.validCharacters();
+        int randomPos = (int) Math.floor(Math.random() * validChars.length());
+        String oneChar = validChars.substring(randomPos, 1);
+        return this.reverceConvert(oneChar);
+    }
+
+    public static boolean equalInvalidChar(String oneChar) {
+        return oneChar == String.valueOf(CodeToCharacterConverter.INVALID_CHAR);
+    }
+
+    public boolean isInvalidPassword(String text) {
+        if (text.trim().length() == 0) return false;
+        return Arrays.stream(text.split(""))
+                .anyMatch(oneChar -> isInvalidChar(oneChar));
+    }
+
+    public String fixValidPassword(String text) {
+        if (text.trim().length() == 0) return "";
+        return Arrays.stream(text.split(""))
+                .map(oneChar -> fixValidOneChar(oneChar))
+                .collect(joining());
+    }
+
+    private String fixValidOneChar(String oneChar) {
+        if (!this.isInvalidChar(oneChar)) return oneChar;
+        String upperChar = oneChar.toUpperCase();
+        if (!this.isInvalidChar(upperChar)) return upperChar;
+        return "";
+    }
+
+    public String validCharacters() {
+        String invalidCharString = String.valueOf(INVALID_CHAR);
+        return Stream.of(charTable.split(""))
+                .filter(c -> !c.equals(invalidCharString))
+                .collect(joining());
     }
 
     public CodeToCharacterConverter() {
