a024a85152714e8f69a652d95e6fd0cb27993382
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-01-04 02:58:37 +0900

    degug: いんふら層に、ダミー実装を施す.

--- youkai-front/src/infrastructure/transfer/youkai/foundpassword/FoundPasswordTransfer.ts
+++ youkai-front/src/infrastructure/transfer/youkai/foundpassword/FoundPasswordTransfer.ts
@@ -2,11 +2,27 @@ import FoundPasswordRepository from "@/domain/youkai/foundpassword/FoundPassword
 import FoundPasswordSearchCondition from "@/domain/youkai/foundpassword/FoundPasswordSearchCondition";
 import FoundPasswords from "@/domain/youkai/foundpassword/FoundPasswords";
 import { NuxtAxiosInstance } from "@nuxtjs/axios";
+import CodeToCharacterConverter from "~/domain/youkai/checkdigit/converter/CodeToCharacterConverter";
+import Password from "~/domain/youkai/checkdigit/state/Password";
 
 export default class FoundPasswordTransfer implements FoundPasswordRepository {
-    constructor(private readonly axios: NuxtAxiosInstance) {}
+    constructor(private readonly axios: NuxtAxiosInstance) { }
 
     public findOf(condition: FoundPasswordSearchCondition): FoundPasswords {
-        return new FoundPasswords(["ABCDEFGH", "IJKLMNOPQ"] , 2);
+        // TODO 実際のAPI呼び出し。
+
+        // 以下、ダミーコード
+        let fullCount = 0;
+        if (condition.query === "AB") fullCount = 56789;
+        if (condition.query === "XXX") fullCount = 12345;
+        if (condition.query === "MIURA") fullCount = 543;
+
+        const remainCount = Math.min(condition.limit, fullCount - condition.offset)
+        const converter = new CodeToCharacterConverter();
+        const result: string[] = [];
+        for (let i = 0; i < remainCount; i++) {
+            result.push(Password.generateRandom(14).toString());
+        }
+        return new FoundPasswords(result, fullCount);
     }
 }
