d3daa1d1b1adc7b48917337b632baf5660c2c0ad
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-01-03 23:17:02 +0900

    implement: 下までくると、読み直す実装サンプル。

--- youkai-front/src/components/foundpasswordsearch/FoundPasswordSearchResult.vue
+++ youkai-front/src/components/foundpasswordsearch/FoundPasswordSearchResult.vue
@@ -15,34 +15,23 @@
         </v-row>
         <v-row>
           <v-col cols="12" sm="12" md="12">
-            <v-simple-table 
-              dense
-              height="670px"
-              fixed-header
-            >
-                <template v-slot:default>
-                  <thead>
-                    <tr>
-                      <th class="text-left">
-                        No.
-                      </th>
-                      <th class="text-left">
-                        パスワード
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr
-                      v-for="item in list"
-                      :key="item.name"
-                    >
-                      <td>{{ item.no }}</td>
-                      <td>{{ item.password }}</td>
-                    </tr>
-                  </tbody>
-                </template>
-              </v-simple-table>
-            </v-col>
+            <div class="infinite-scroll">
+              <ul class="infinite-scroll-list">
+                <li class="infinite-scroll-list-item" 
+                  v-for="i in this.count" 
+                  :key="i"
+                >
+                  scroll {{ i }}
+                </li>
+              </ul>
+              <infinite-loading 
+                ref="infiniteLoading" 
+                spinner="spiral"
+                @infinite="infiniteHandler">
+                <div slot="no-results"/>
+              </infinite-loading>
+            </div>
+          </v-col>
         </v-row>
       </v-container>
     </v-form>
@@ -51,15 +40,22 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import InfiniteLoading from 'vue-infinite-loading'
 
-@Component
+@Component({
+  components: {
+    InfiniteLoading,
+  },
+})
 export default class FoundPasswordSearchResult extends Vue {
   private fullCount = 0
+
+  private count = 20
 
   private readonly list: any[] = []
 
   private mounted(): void {
-    const list = this.list;
+    const list = this.list
     for (let i = 0; i < 50000; i++) {
       list.push({
         no: i + 1,
@@ -67,11 +63,34 @@ export default class FoundPasswordSearchResult extends Vue {
       })
     }
   }
+
+  private infiniteHandler() {
+    setTimeout(() => {
+      this.count += 20
+      const infiniteLoading = this.$refs.infiniteLoading as InfiniteLoading
+      infiniteLoading.stateChanger.loaded()
+    }, 1000)
+  }
 }
 </script>
 
 <style scoped>
 .input-yokai-password {
   ime-mode: disabled;
+}
+
+.infinite-scroll {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 680px;
+  margin: 100px 0;
+}
+
+.infinite-scroll-list-item {
+  height: 60px;
+  margin: 10px 0;
+  border-bottom: 1px solid #eaeaea;
+  padding-bottom: 10px;
 }
 </style>
