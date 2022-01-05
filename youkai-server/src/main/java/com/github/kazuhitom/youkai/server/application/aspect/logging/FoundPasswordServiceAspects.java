e9a310478912ac5998ea306d8259c14c035acda5
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-01-05 19:32:41 +0900

    implement: AOPが使えるように設定。

---
+++ youkai-server/src/main/java/com/github/kazuhitom/youkai/server/application/aspect/logging/FoundPasswordServiceAspects.java
@@ -0,0 +1,9 @@
+package com.github.kazuhitom.youkai.server.application.aspect.logging;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class FoundPasswordServiceAspects {
+}
