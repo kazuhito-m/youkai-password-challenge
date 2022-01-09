897e4a3e939aa4d8277679d856430b98b4044464
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-01-10 01:40:21 +0900

    debug: axiosをNuxt用以外のものに差し替えてみる。

--- youkai-front/src/infrastructure/transfer/youkai/password/hazure/HazurePasswordTransfer.ts
+++ youkai-front/src/infrastructure/transfer/youkai/password/hazure/HazurePasswordTransfer.ts
@@ -1,16 +1,17 @@
 // ignore lint rule becouse axios bug. see: https://github.com/nuxt-community/axios-module/issues/555
 /* eslint import/named: 0 */
-import { NuxtAxiosInstance } from "@nuxtjs/axios";
+// import { NuxtAxiosInstance } from "@nuxtjs/axios";
+import axios from 'axios';
 import RegisterHazurePasswordsResponse from "./RegisterHazurePasswordsResponse";
 import HazurePasswordRepository from "@/domain/youkai/password/hazure/HazurePasswordRepository";
 
 export default class HazurePasswordTransfer implements HazurePasswordRepository {
-    constructor(private readonly axios: NuxtAxiosInstance) { }
+    // constructor(private readonly axios: NuxtAxiosInstance) { }
 
     public async register(passwords: string[]): Promise<boolean> {
         try {
             const url = "https://yokai-server-vunudhkwpa-an.a.run.app/api/import/rejected";
-            const response = await this.axios.post<RegisterHazurePasswordsResponse>(url, passwords);
+            const response = await axios.post<RegisterHazurePasswordsResponse>(url, passwords);
             const data = response.data;
             console.log(response);
             return data.rejected > 0;
