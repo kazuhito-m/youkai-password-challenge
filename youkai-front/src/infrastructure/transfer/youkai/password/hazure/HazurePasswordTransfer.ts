50144319426b96eaf2484480a97a0d41a11e9dc9
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-01-10 18:02:39 +0900

    implement: debug用のログを除去。

--- youkai-front/src/infrastructure/transfer/youkai/password/hazure/HazurePasswordTransfer.ts
+++ youkai-front/src/infrastructure/transfer/youkai/password/hazure/HazurePasswordTransfer.ts
@@ -13,7 +13,6 @@ export default class HazurePasswordTransfer implements HazurePasswordRepository 
             const url = "https://yokai-server-vunudhkwpa-an.a.run.app/api/import/rejected";
             const response = await axios.post<RegisterHazurePasswordsResponse>(url, passwords);
             const data = response.data;
-            console.log(response);
             return data.rejected >= 0;
         } catch (error: any) {
             console.log("通信時エラー", error);
