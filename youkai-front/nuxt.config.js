6d2329579d3c7d768068f263babe50adbd8c40a1
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-10-09 02:49:59 +0900

    implement: アンカーリンクの色を見やすい色に変更する。 fixed #20

--- youkai-front/nuxt.config.js
+++ youkai-front/nuxt.config.js
@@ -83,7 +83,8 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          success: colors.green.accent3,
+          anchor: '#49a6e2',
         }
       }
     }
