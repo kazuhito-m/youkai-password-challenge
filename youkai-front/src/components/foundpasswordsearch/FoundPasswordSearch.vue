1e2971586511c85669026f3f3f1ce117a2680937
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-10-08 23:58:56 +0900

    refactor: 広告の位置調整と同時に、コンポーネント化。

--- youkai-front/src/components/foundpasswordsearch/FoundPasswordSearch.vue
+++ youkai-front/src/components/foundpasswordsearch/FoundPasswordSearch.vue
@@ -2,15 +2,7 @@
   <v-container>
     <v-row justify="center" align="center">
       <v-col cols="12" sm="6" md="6">
-        <v-card>
-          <div>
-            <adsbygoogle
-              :ad-slot="2708086751"
-              :ad-style="{ display: 'block' }"
-              :ad-format="'auto'"
-            />
-          </div>
-        </v-card>
+        <AdSideways />
         <FoundPasswordSearchParameter />
       </v-col>
       <v-col cols="12" sm="6" md="6">
@@ -24,11 +16,13 @@
 import { Component, Vue } from 'vue-property-decorator'
 import FoundPasswordSearchParameter from '@/components/foundpasswordsearch/FoundPasswordSearchParameter.vue'
 import FoundPasswordSearchResult from '@/components/foundpasswordsearch/FoundPasswordSearchResult.vue'
+import AdSideways from '@/components/googleadsense/AdSideways.vue'
 
 @Component({
   components: {
     FoundPasswordSearchParameter,
     FoundPasswordSearchResult,
+    AdSideways,
   },
 })
 export default class FoundPasswordSearch extends Vue {}
---
+++ youkai-front/src/components/googleadsense/AdSideways.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <adsbygoogle
+      :ad-slot="2708086751"
+      :ad-style="{ display: 'block' }"
+      :ad-format="'auto'"
+    />
+  </div>
+</template>
